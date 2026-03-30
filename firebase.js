80a3cbdcdfc860f3889a4734ae0bacb39d03af0b
star17088 <star17088@gmail.com>
2026-03-30 15:39:14 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,38 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+// Firebase CDN 방식
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  onSnapshot,
+  updateDoc,
+  doc,
+  deleteDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+// 🔥 당신 Firebase 설정 그대로 넣기
 const firebaseConfig = {
   apiKey: "AIzaSyADGR45Y4rzrLsqk7dt05iCIu2dONSE3NI",
   authDomain: "ghostpang.firebaseapp.com",
   projectId: "ghostpang",
   storageBucket: "ghostpang.firebasestorage.app",
   messagingSenderId: "494956996773",
-  appId: "1:494956996773:web:b3346f2116e88d8192054c",
+  appId: "1:494956996773:web:b3346f2116e88d8192054c"
 };
 
+// 초기화
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const db = getFirestore(app);
+
+// 👉 export
+export {
+  db,
+  collection,
+  addDoc,
+  getDocs,
+  onSnapshot,
+  updateDoc,
+  doc,
+  deleteDoc
+};
